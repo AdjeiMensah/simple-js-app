5587a299120f08930758c825c0e3eeca364147c1
AdjeiMensah <48394224+AdjeiMensah@users.noreply.github.com>
2023-01-22 15:46:43 +0100

    pokemonList
    pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello world');
-let favoriteFood = 'Plantain';
-document.write(favoriteFood)
+let pokemonList = [
+    {name: 'Articuno', height: 1.7 _types: ['Ice', 'Flying'],
+get types() {
+            return this._types;
+        },
+set types(value) {
+            this._types = value;
+        },
+},
+    {name: 'Snorlax', height: 2.1 types: ['normal']},
+    {name: 'Gengar', height: 1.5 types: ['Ghost','Poison']},
+];
+    
