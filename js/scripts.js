8c8731ae7fc77cc0684d80d0478f7b1992e36504
AdjeiMensah <48394224+AdjeiMensah@users.noreply.github.com>
2023-01-31 16:49:51 +0100

    Pokemon-list-array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 let pokemonList = [
-    {name: 'Articuno', height: 1.7 types: ['Ice','Flying']},
-    {name: 'Snorlax', height: 2.1 types: ['normal']},
-    {name: 'Gengar', height: 1.5 types: ['Ghost','Poison']},
+    {name: 'Articuno', height: 1.7, types: ['Ice','Flying']},
+    {name: 'Snorlax', height: 2.1, types: ['normal']},
+    {name: 'Gengar', height: 1.5, types: ['Ghost','Poison']},
 ];
     
+for (let i=0; i < pokemonList.length; i++){
+if(pokemonList[i].height>2){
+    document.write(pokemonList[i].name + " (height: " +pokemonList[i].height+") "+ "Wow, thats big!") ;
+}
+else{
+    document.write(pokemonList[i].name + " (height: " +pokemonList[i].height+")" )
+}
+    }
