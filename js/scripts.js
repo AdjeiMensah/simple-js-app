7bb357095744c955fb2f3ef116892d4506489132
AdjeiMensah <48394224+AdjeiMensah@users.noreply.github.com>
2023-03-10 20:19:51 +0100

    Added Pokedex and created Event Listener to every pokemon button

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
-
 let pokemonRepository = (function () {
-let pokemonList = [
-    {name: 'Articuno', height: 1.7, types: ['Ice','Flying']},
-    {name: 'Snorlax', height: 2.1, types: ['normal']},
-    {name: 'Gengar', height: 1.5, types: ['Ghost','Poison']},
-
-];
-      
-        return {
-          add: function(pokemon) {
-            pokemonList.push(pokemon);
-          },
-          getAll: function() {
-            return pokemonList;
-          }
-        };
-      })();
-    
-
-function printdetails(pokemon) {
-    if(pokemon.height>2){
-        document.write(pokemon.name + " (height: " +pokemon.height+") "+ "Wow, thats big!") ;
+  let pokemonList = [
+      {name: 'Articuno', height: 1.7, types: ['Ice','Flying']},
+      {name: 'Snorlax', height: 2.1, types: ['normal']},
+      {name: 'Gengar', height: 1.5, types: ['Ghost','Poison']},
+  ];
+        
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    },
+    showDetails: function(pokemon) {
+      console.log(pokemon);
+    },
+    addListItem: function(pokemon) {
+      let pokemonList = document.querySelector('.pokemon-list');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add("pokemon-button");
+      button.addEventListener('click', function() {
+        pokemonRepository.showDetails(pokemon);
+      })
+      listItem.appendChild(button);
+      pokemonList.appendChild(listItem);
     }
-    else{
-        document.write(pokemon.name + " (height: " +pokemon.height+")" )
-    }
-}
-pokemonRepository.getAll().forEach(printdetails);
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
