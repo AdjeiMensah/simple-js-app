7a628b511d4f9ba2b311a4c2ad84bb7bb5c61fa2
AdjeiMensah <48394224+AdjeiMensah@users.noreply.github.com>
2023-03-24 08:14:28 +0100

    load some data from an external API and display it to your users

--- js/scripts.js
+++ js/scripts.js
@@ -72,8 +72,65 @@ const pokemonRepository = (() => {
 
   // Function for showing the details of a specific Pokemon in the console
   function showDetails(pokemon) {
-    console.log(pokemon);
+    showModal(pokemon);
   }
+
+  function showModal(pokemon) {
+    let modalContainer = document.querySelector('#modal-container');
+
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let myImage = document.createElement('img');
+    myImage.setAttribute('src', pokemon.imageUrl);
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: ' + pokemon.height;
+
+
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(myImage);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+
+
+    modalContainer.classList.add('is-visible');
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+    });
+
 
   // Return an object containing the add, getAll, and addListItem methods, as well as the loadList and loadDetails functions
   return {
